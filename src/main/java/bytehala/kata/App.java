245bb2374062def1a63ea5803581e6eb126beabc
Lem Dulfo <lemuel.dulfo@gmail.com>
2016-12-26 01:05:55 +0800

    Adding beanio csv file to resources

--- src/main/java/bytehala/kata/App.java
+++ src/main/java/bytehala/kata/App.java
@@ -4,10 +4,10 @@ package bytehala.kata;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        String fileName = "beanbag.csv"
+        System.out.println( "Divesting file: " + fileName );
+        // TODO Pass the fileName to the divester class
     }
 }
