2907bfb4cbb44c8d78cd0457f3dd5e5a96fa48fc
Lem Dulfo <lemuel.dulfo@gmail.com>
2016-12-26 18:18:44 +0800

    Now reads beans from file

--- src/main/java/bytehala/kata/App.java
+++ src/main/java/bytehala/kata/App.java
@@ -1,13 +1,38 @@
 package bytehala.kata;
 
-/**
- * Hello world!
- *
- */
+import bytehala.kata.models.PointCard;
+import org.beanio.BeanReader;
+import org.beanio.StreamFactory;
+import java.io.File;
+
+
 public class App {
     public static void main( String[] args ) {
-        String fileName = "beanbag.csv";
+        String fileName = "src/main/resources/" + "beanbag.csv";
         System.out.println( "Divesting file: " + fileName );
-        // TODO Pass the fileName to the divester class
+
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+
+        StreamFactory factory = StreamFactory.newInstance();
+        // load the mapping file from the working directory
+        factory.load("src/main/resources/" + "pointcards.xml");
+
+        // create a BeanReader to read from "input.csv"
+        BeanReader in = factory.createReader("pointcards", new File(fileName));
+        // create a BeanWriter to write to "output.csv"
+        //BeanWriter out = factory.createWriter("contacts", new File("output.csv"));
+
+        Object record = null;
+
+        while ((record = in.read()) != null) {
+            if ("pointcard".equals(in.getRecordName())) {
+                PointCard pointcard = (PointCard) record;
+                System.out.println(pointcard.getSerialNumber());
+                // process the contact...
+            }
+        }
+
+        in.close();
     }
 }
