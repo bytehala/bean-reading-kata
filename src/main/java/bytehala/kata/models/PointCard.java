804056e9d252b04142bb6f9c157b49948a802388
Lem Dulfo <lemuel.dulfo@gmail.com>
2016-12-26 17:28:32 +0800

    Added model for PointCard

---
+++ src/main/java/bytehala/kata/models/PointCard.java
@@ -0,0 +1,31 @@
+package bytehala.kata.models;
+
+public final class PointCard {
+  String serialNumber;
+  int numPoints;
+  String countryAlpha2Code;
+
+  public void setSerialNumber(String serialNumber) {
+    this.serialNumber = serialNumber;
+  }
+
+  public String getSerialNumber() {
+    return serialNumber;
+  }
+
+  public void setNumPoints(int numPoints) {
+    this.numPoints = numPoints;
+  }
+
+  public int getNumPoints() {
+    return numPoints;
+  }
+
+  public void setCountryAlpha2Code(String alpha2) {
+    countryAlpha2Code = alpha2;
+  }
+
+  public String getCountryAlpha2Code() {
+    return countryAlpha2Code;
+  }
+}
