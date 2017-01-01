bfd0dcb1904064617f478c4174c1a197822764ad
Lem Dulfo <lemuel.dulfo@gmail.com>
2017-01-01 21:22:21 +0800

    PointCardReaderTest class

---
+++ src/test/java/bytehala/kata/reader/PointCardReaderTest.java
@@ -0,0 +1,13 @@
+package bytehala.kata.reader;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PointCardReaderTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
